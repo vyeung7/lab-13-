10a751ab2048dfe5668b8f36e6658b3d9f068f77
vyeung7 <Starloop2003@gmail.com>
2023-04-20 22:37:38 -0400

    Update script.js

--- script.js
+++ script.js
@@ -7,14 +7,14 @@ function start(){
 
 function change(){
    var changeInColor= 'pink';
+   var button=document.getElementById("img");
 //new background color and fontweight
   document.getElementById('title').style.fontSize='40px';
   document.getElementById('title').style.font='times';
   document.getElementById('title').style.color= 'purple';
   document.getElementById('title').style.fontWeight="bold";
   document.body.style.backgroundColor= changeInColor;
-  //add hello kitty gif
-    var button=document.getElementById("img");
+   //add hello kitty gif
   button.setAttribute("src", "kitty.gif");
 
 }
