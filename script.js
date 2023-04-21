df48973c15c4f23e13c68359aab7c7d374bc2454
vyeung7 <Starloop2003@gmail.com>
2023-04-20 22:30:35 -0400

    new

--- script.js
+++ script.js
@@ -1,28 +1,22 @@
 function start(){
     var button=document.getElementById('press');
     button.onclick=change;
-    button.onclick=validateField;
     document.body.style.textAlign= 'center';
+    //centered text and button
 }
 
 function change(){
    var changeInColor= 'pink';
-
-
+//new background color and fontweight
   document.getElementById('title').style.fontSize='40px';
   document.getElementById('title').style.font='times';
   document.getElementById('title').style.color= 'purple';
   document.getElementById('title').style.fontWeight="bold";
   document.body.style.backgroundColor= changeInColor;
-  
+  //add hello kitty gif
+    var button=document.getElementById("img");
+  button.setAttribute("src", "kitty.gif");
+
 }
-
-function validateField() {
-
-    var docs = document.getElementById("img");
-    docs.setAttribute("src", "gif_path");
-    
-    }
-
 
 window.onload=start;
