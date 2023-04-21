f1d7ee653a0d75a6e4d7748803bf9139db140351
vyeung7 <Starloop2003@gmail.com>
2023-04-20 22:05:31 -0400

    Update script.js

--- script.js
+++ script.js
@@ -4,9 +4,16 @@ function start(){
 }
 
 function change(){
+   var changeInColor= 'pink';
+
   document.getElementById('title').style.fontSize='40px';
   document.getElementById('title').style.font='times';
-  document.getElementById('title').style.color='purple';
+  document.getElementById('title').style.color= 'purple';
+  document.getElementById('title').style.fontWeight="bold";
+  document.body.style.backgroundColor= changeInColor;
+  
+
+  
 }
 
 window.onload=start;
